d057756d8edf12750705c88043406f93c7e1f829
Devsk001 <srik3713@gmail.com>
2023-04-16 23:14:36 +0530

    mobile bg updated by devsk

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
- console.log("The Kashmir Tourism")
+console.log("The Kashmir Tourism");
+document.addEventListener("contextmenu", (event) => {
+         event.preventDefault();
+      });
+// date update js code //
+const date=new Date();
+console.log(date);
+const year=date.getFullYear();
+console.log(year);
+// document.getElementById('year').innerHTML = year+1;
+ var check =document.getElementById('year').innerHTML=year;
+// end of the year update //
