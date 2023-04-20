19c0243ea687e90642e9fe82aa91c099faf9d169
siththarth <sevigounder1802@gmail.com>
2023-04-20 13:09:24 +0530

    the will modifies the design

--- index.js
+++ index.js
@@ -28,31 +28,32 @@ $(document).ready(function () {
 
 //   cars slide
 $(document).ready(function () {
-    for (var i = 1; i <= $(".slider__slide").length; i++) {
-      $(".slider__indicators").append(
-        '<div class="slider__indicator" data-slide="' + i + '"></div>'
+    for (var i = 1; i <= $(".slider__slide01").length; i++) {
+      $(".slider__indicators01").append(
+        '<div class="slider__indicator01" data-slide="' + i + '"></div>'
       );
     }
     setTimeout(function () {
-      $(".slider__wrap").addClass("slider__wrap--hacked");
+      $(".slider__wrap01").addClass("slider__wrap--hacked01");
     }, 1000);
   });
   
-  function goToSlide(number) {
-    $(".slider__slide").removeClass("slider__slide--active");
-    $(".slider__slide[data-slide=" + number + "]").addClass(
-      "slider__slide--active"
+  function goToSlides(number) {
+    $(".slider__slide01").removeClass("slider__slide--active01");
+    $(".slider__slide01[data-slide=" + number + "]").addClass(
+      "slider__slide--active01"
     );
   }
   
-  $(".slider__next, .next-nxt").on("click", function () {
-    var currentSlide = Number($(".slider__slide--active").data("slide"));
-    var totalSlides = $(".slider__slide").length;
+  $(".slider__next01, .go-to-next01").on("click", function () {
+    var currentSlide = Number($(".slider__slide--active01").data("slide"));
+    var totalSlides = $(".slider__slide01").length;
     currentSlide++;
     if (currentSlide > totalSlides) {
       currentSlide = 1;
     }
-    goToSlide(currentSlide);
+    goToSlides(currentSlide);
   });
 
-
+var check=document.getElementsByClassName('.go-to-01');
+console.log(check);
