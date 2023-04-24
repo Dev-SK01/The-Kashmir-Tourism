c2888894df01a75a4422c591cee0706a7b672832
Devsk001 <srik3713@gmail.com>
2023-04-25 00:43:50 +0530

    user select disabled final product:)

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ $(document).ready(function () {
   const year=date.getFullYear();
   console.log(year);
   // document.getElementById('year').innerHTML = year+1;
-   var check =document.getElementById('year').innerHTML=year;
+   var update =document.getElementById('year').innerHTML=year;
   // end of the year update //
